88152452e51eecf384b99cdcd4679532a8e764bc
zou_home_PC <zoujiahuibin@outlook.com>
2023-04-16 23:55:33 +0800

    add ModelSetup PoissonMA

---
+++ R/Modelsetup.R
@@ -0,0 +1,79 @@
+#' @title ModelSetup: Setup the index matrix of variables of candidate models
+#' @description
+#' ModelSetup is a function to generate an index matrix of variables of candidate models.
+#' The sth row contains the indexes of variables used in the sth candidate model.
+#'
+#' @param y a vector consist of natural numbers.
+#' @param X a numeric matrix.
+#' @param modeltype a string chose from 'nested' or 'all'. 'nested' means
+#' generating candidate models in nested from according to cor(y,x); 'all' means considering
+#' all possible combinations.
+#' @param intercept a bool. It means the first column of X is 1, or not.
+#'
+#' @return an index matrix of variables of candidate models. The sth row contains
+#' the indexes of variables used in the sth candidate model.
+#' @export
+#'
+#' @examples
+#' library(mvtnorm)
+#' beta0=rep(0.5,5)
+#' X=rmvnorm(100,mean=rep(0,5))
+#' lambda=exp(X%*%beta0)
+#' y=rpois(100,lambda)
+#' ModelSetup(y,X,'nested',intercept=F)
+#'
+#'
+#' beta0=rep(0.5,5)
+#' X=cbind(1,rmvnorm(100,mean=rep(0,4)))
+#' lambda=exp(X%*%beta0)
+#' y=rpois(100,lambda)
+#' ModelSetup(y,X,'nested',intercept=T)
+#'
+#'
+#'
+library(magrittr)
+ModelSetup<-function(y,X,modeltype='nested',intercept=F){
+  if(modeltype=='all'){
+    if(intercept){
+      m=ncol(X)-1
+      Index=matrix(ncol=m,nrow=0)
+      for (i in 1:m) {
+        index=combn(m,i)+1
+        index=rbind(index,matrix(0,nrow=m-i,ncol=choose(m,i)))
+        Index=rbind(Index,t(index))
+      }
+      Index=cbind(1,Index)
+
+    }else{
+      m=ncol(X)
+      Index=matrix(ncol=m,nrow=0)
+      for (i in 1:m) {
+        index=combn(m,i)
+        index=rbind(index,matrix(0,nrow=m-i,ncol=choose(m,i)))
+        Index=rbind(Index,t(index))
+      }
+    }
+
+  }else if(modeltype=='nested'){
+    if(intercept){
+      X2=cbind(y,X[,2:ncol(X)])
+      orders=X2%>%cor%>%.[1,2:ncol(X2)]%>%abs%>%order(decreasing = T)%>%add(1)
+      p=ncol(X)
+      m=p-1
+      Index=matrix(0,ncol=m,nrow=m)
+      for (i in 1:m) {
+        Index[i,1:i]=orders[1:i]
+      }
+      Index%<>%cbind(matrix(rep(1,each=m),nrow=m),.)
+    }else{
+      X2=cbind(y,X)
+      orders=X2%>%cor%>%.[1,2:ncol(X2)]%>%abs%>%order(decreasing = T)
+      m=ncol(X)
+      Index=matrix(0,ncol=m,nrow=m)
+      for (i in 1:m) {
+        Index[i,1:i]=orders[1:i]
+      }
+    }
+  }
+  return(Index)
+}
